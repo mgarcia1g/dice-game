88a58d414586ca85d325e13c155be5115c14eb74
mgarcia1g <55240072+mgarcia1g@users.noreply.github.com>
2022-12-11 22:23:55 -0500

    Add files via upload
    Original Code

---
+++ index.js
@@ -0,0 +1,13 @@
+function rollDice(){
+    let player1Roll = Math.floor(Math.random() * 6)+1;
+
+    let player2Roll = Math.floor(Math.random() * 6)+1;
+  
+
+     document.querySelector(".player1Dice").src="./images/dice"+player1Roll+".png";
+     document.querySelector(".player2Dice").src="./images/dice"+player2Roll+".png";
+     document.querySelector("button").innerHTML="Try again!"
+
+     player1Roll > player2Roll ?  document.querySelector(".winner").innerHTML="Player 1 Wins!" : document.querySelector(".winner").innerHTML=" Player 2 Wins!";
+
+}
