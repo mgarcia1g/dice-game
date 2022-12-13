a2d4f01053c7a8af8f9dd1dd5ff1b5bb725c66d1
mgarcia1g <55240072+mgarcia1g@users.noreply.github.com>
2022-12-12 20:13:27 -0500

    Add files via upload
    Added condition to check if its a tie on roll

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ function rollDice(){
      document.querySelector(".player1Dice").src="./images/dice"+player1Roll+".png";
      document.querySelector(".player2Dice").src="./images/dice"+player2Roll+".png";
      document.querySelector("button").innerHTML="Try again!"
+    
+     let clearWinner = player1Roll > player2Roll ?  document.querySelector(".winner").innerHTML="Player 1 Wins!" : document.querySelector(".winner").innerHTML=" Player 2 Wins!";
 
-     player1Roll > player2Roll ?  document.querySelector(".winner").innerHTML="Player 1 Wins!" : document.querySelector(".winner").innerHTML=" Player 2 Wins!";
+     player1Roll === player2Roll ? document.querySelector(".winner").innerHTML="Its a tie!!!" : clearWinner;
+
 
 }
